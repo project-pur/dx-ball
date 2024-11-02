568999a3de68db5fe02155a0baca9c8ac3e1bcce
Nayeem Ahmed <114806993+Nayeem-Ahmed1@users.noreply.github.com>
2024-11-02 20:56:18 +0600

    function update

--- dx ball.c
+++ dx ball.c
@@ -1,3 +1,19 @@
+#include <stdio.h>
+#include <conio.h>   // For getch()
+#include <windows.h> // For Sleep()
+
+#define WIDTH 40
+#define HEIGHT 20
+#define PADDLE_WIDTH 6
+#define MAX_LIVES 3
+
+int ball_x, ball_y;
+int ball_dx = 1, ball_dy = 1;
+int paddle_x = WIDTH / 2 - PADDLE_WIDTH / 2;
+int score = 0;
+int lives = MAX_LIVES;
+char bricks[HEIGHT / 2][WIDTH];
+
 // Function to initialize bricks in structured rows
 void init_bricks() {
     for (int i = 0; i < HEIGHT / 2; i++)
